7dd516fdb1b9378676931b3aed5bbf8b8977ae05
Marsel Brandao Junior <marseljrdev@gmail.com>
2023-01-10 15:24:40 -0300

    Pagina web dinamica e buttao de cliques

---
+++ exercicio2.js
@@ -0,0 +1,36 @@
+// 2. Crie uma página que tenha o título "Contador de Cliques". Na
+//    página deve ter 2 botões, um deles deve estar escrito INCREMENTAR
+//    e o outro deve estar escrito ZERAR CONTADOR, logo abaixo dos
+//    botões deve ter um parágrafo contendo a seguinte frase "O
+//    contador está com 0 cliques".
+
+//    Conforme o usuário vai clicando no botão Incrementar deve ser
+//    incrementado uma variável no javascript e ir atualizando a página
+//    conforme o número de cliques.
+
+//    Ao clicar no botão Zerar contador o parágrafo deve sumir da tela.
+//    Ao clicar no botão de Incrementar novamente o parágrafo deve
+//    aparecer com a mensagem "O contador está com 1 cliques" e ir
+//    atualizando conforme o usuário for clicando para incrementar.
+
+
+let count = 0;
+
+function incrementarClick(){
+    atualizaPagina(++count);
+}
+
+function zerarContador(){
+    count = 0;
+    atualizaPagina(count);
+
+}
+
+function atualizaPagina(count){
+    const mostraClick = document.getElementById('exibeClick')
+
+    mostraClick.innerText = '';
+
+    mostraClick.innerHTML = `O contador esta com ${count} cliques`;
+
+}
